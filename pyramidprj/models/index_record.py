9768f88c714192d039704e0ad74db59ab4febe13
js <35079323+santosoj@users.noreply.github.com>
2022-12-31 14:25:23 +1100

    rm MyModel / add IndexRecord model

---
+++ pyramidprj/models/index_record.py
@@ -0,0 +1,14 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+)
+
+from .meta import Base
+
+
+class IndexRecord(Base):
+    __tablename__ = 'index_record'
+    id = Column(Integer, primary_key=True)
+    date = Column(Text)
+    body = Column(Text)
